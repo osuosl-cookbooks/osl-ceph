40842bd06acdf7c5d8bf25aec442b7d3c8506ef9
armiller <armiller@osuosl.org>
2015-09-25 15:16:02 -0700

    init commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+osl-ceph CHANGELOG
+==================
+This file is used to list changes made in each version of the
+osl-ceph cookbook.
+
+0.1.0
+-----
+- Initial release of osl-ceph
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'osl-ceph'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apache2'
+description      'Installs/Configures osl-ceph'
+long_description 'Installs/Configures osl-ceph'
+version          '0.1.0'
+
