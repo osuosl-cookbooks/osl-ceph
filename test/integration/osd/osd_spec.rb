e7d8e0bf5d748afe339c6d292b7e4c6e8d52274e
Lance Albertson <lance@osuosl.org>
2018-02-05 14:26:29 -0800

    Don't test for osd services themselves being enabled
    That's taken care of via the target service

--- test/integration/osd/osd_spec.rb
+++ test/integration/osd/osd_spec.rb
@@ -6,15 +6,8 @@ describe command('ss -tpln') do
   its('stdout') { should include 'ceph-osd' }
 end
 
-%w(
-  ceph-osd.target
-  ceph-osd@0.service
-  ceph-osd@1.service
-  ceph-osd@2.service
-).each do |s|
-  describe service(s) do
-    it { should be_installed }
-    it { should be_enabled }
-    it { should be_running }
-  end
+describe service('ceph-osd.target') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
