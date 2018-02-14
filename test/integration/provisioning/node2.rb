0071c7d4170f4e8c4fb3f7f2010d65a61e875bc8
Lance Albertson <lance@osuosl.org>
2018-02-14 09:51:27 -0800

    Remove vagrant provisioning and update docs for provisioning testing

--- test/integration/provisioning/node2.rb
+++ test/integration/provisioning/node2.rb
@@ -1,24 +1,12 @@
 require 'chef/provisioning'
 
-node_os = ENV['NODE_OS'] || 'bento/centos-7.4'
-flavor_ref = ENV['FLAVOR'] || 4 # m1.large
-
-unless ENV['CHEF_DRIVER'] == 'fog:OpenStack'
-  require 'chef/provisioning/vagrant_driver'
-  vagrant_box node_os
-  with_driver "vagrant:#{File.dirname(__FILE__)}/../../../vms"
-end
-
 machine 'node2' do
-  machine_options vagrant_options: {
-    'vm.box' => node_os
+  machine_options bootstrap_options: {
+    image_ref: ENV['NODE_OS'],
+    flavor_ref: ENV['FLAVOR'],
+    key_name: ENV['OS_SSH_KEYPAIR'],
+    nics: [{ net_id: ENV['OS_NETWORK_UUID'] }]
   },
-                  bootstrap_options: {
-                    image_ref: node_os,
-                    flavor_ref: flavor_ref,
-                    key_name: ENV['OS_SSH_KEYPAIR'],
-                    nics: [{ net_id: ENV['OS_NETWORK_UUID'] }]
-                  },
                   ssh_username: 'centos',
                   ssh_options: {
                     key_data: nil
@@ -28,13 +16,6 @@ machine 'node2' do
                   }
 
   ohai_hints 'openstack' => '{}'
-  add_machine_options vagrant_config: <<-EOF
-config.vm.network "private_network", ip: "192.168.60.11"
-config.vm.provider "virtualbox" do |v|
-  v.memory = 4096
-  v.cpus = 2
-end
-EOF
   attribute %w(ceph_test openstack_auth_url), "#{ENV['OS_AUTH_URL']}/tokens"
   attribute %w(ceph_test openstack_username), ENV['OS_USERNAME']
   attribute %w(ceph_test openstack_api_key), ENV['OS_PASSWORD']
@@ -43,8 +24,6 @@ EOF
   role 'ceph_mon'
   role 'ceph_mgr'
   role 'ceph_osd'
-  file('/etc/chef/encrypted_data_bag_secret',
-       File.dirname(__FILE__) +
-       '/../encrypted_data_bag_secret')
+  file('/etc/chef/encrypted_data_bag_secret', File.dirname(__FILE__) + '/../encrypted_data_bag_secret')
   converge true
 end
