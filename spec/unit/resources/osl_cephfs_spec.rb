7cbacb52e846e4d1acdd7f65ff676f4603d8c8e6
Lance Albertson <lance@osuosl.org>
2019-01-02 16:20:56 -0800

    Create osl_cephfs
    The primary difference between this resource and the upstream resource is that
    we aren't dynamically creating and adding the key to the server. This allows us
    to add cephfs mounts without the need of using the admin key on random hosts.

---
+++ spec/unit/resources/osl_cephfs_spec.rb
@@ -0,0 +1,190 @@
+require_relative '../../spec_helper'
+
+describe 'ceph_test::mds' do
+  ALL_PLATFORMS.each do |p|
+    context "#{p[:platform]} #{p[:version]}" do
+      cached(:chef_run) do
+        mon_node = stub_node('mon', p) do |node|
+          node.automatic['fqdn'] = 'ceph-mon.example.org'
+          node.automatic['roles'] = 'search-ceph-mon'
+        end
+        ChefSpec::ServerRunner.new(p.dup.merge(step_into: %w(osl_cephfs))) do |_node, server|
+          server.create_node(mon_node)
+          server.create_data_bag(
+            'ceph',
+            'nagios' => {
+              'nagios_token' => {
+                'x86' => 'x86_key',
+                'ppc64' => 'ppc64_key',
+              },
+            }
+          )
+        end.converge(described_recipe)
+      end
+      include_context 'common_stubs'
+      it 'converges successfully' do
+        expect { chef_run }.to_not raise_error
+      end
+      it do
+        expect(chef_run).to mount_osl_cephfs('/mnt/ceph')
+          .with(
+            key: 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==',
+            client_name: 'cephfs'
+          )
+      end
+      it do
+        expect(chef_run).to create_file('ceph.client secret for /mnt/ceph')
+          .with(
+            path: '/etc/ceph/ceph.client.cephfs.secret',
+            content: "AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==\n",
+            sensitive: true,
+            owner: 'ceph',
+            group: 'ceph',
+            mode: '0600'
+          )
+      end
+      it do
+        expect(chef_run).to create_directory('/mnt/ceph').with(recursive: true)
+      end
+      it do
+        expect(chef_run).to mount_mount('/mnt/ceph')
+          .with(
+            fstype: 'ceph',
+            device: ':/',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to enable_mount('/mnt/ceph')
+          .with(
+            fstype: 'ceph',
+            device: ':/',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to mount_osl_cephfs('/mnt/foo')
+          .with(
+            key: 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==',
+            subdir: '/foo',
+            client_name: 'cephfs'
+          )
+      end
+      it do
+        expect(chef_run).to create_file('ceph.client secret for /mnt/foo')
+          .with(
+            path: '/etc/ceph/ceph.client.cephfs.secret',
+            content: "AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==\n",
+            sensitive: true,
+            owner: 'ceph',
+            group: 'ceph',
+            mode: '0600'
+          )
+      end
+      it do
+        expect(chef_run).to create_directory('/mnt/foo').with(recursive: true)
+      end
+      it do
+        expect(chef_run).to mount_mount('/mnt/foo')
+          .with(
+            fstype: 'ceph',
+            device: ':/foo',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to enable_mount('/mnt/foo')
+          .with(
+            fstype: 'ceph',
+            device: ':/foo',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to mount_osl_cephfs('/mnt/bar')
+          .with(
+            key: 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==',
+            subdir: '/bar',
+            client_name: 'cephfs'
+          )
+      end
+      it do
+        expect(chef_run).to create_file('ceph.client secret for /mnt/bar')
+          .with(
+            path: '/etc/ceph/ceph.client.cephfs.secret',
+            content: "AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==\n",
+            sensitive: true,
+            owner: 'ceph',
+            group: 'ceph',
+            mode: '0600'
+          )
+      end
+      it do
+        expect(chef_run).to create_directory('/mnt/bar').with(recursive: true)
+      end
+      it do
+        expect(chef_run).to mount_mount('/mnt/bar')
+          .with(
+            fstype: 'ceph',
+            device: ':/bar',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to enable_mount('/mnt/bar')
+          .with(
+            fstype: 'ceph',
+            device: ':/bar',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to umount_osl_cephfs('/mnt/bar')
+          .with(
+            key: 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ==',
+            subdir: '/bar',
+            client_name: 'cephfs'
+          )
+      end
+      it do
+        expect(chef_run).to delete_file('ceph.client secret for /mnt/bar')
+          .with(
+            path: '/etc/ceph/ceph.client.cephfs.secret',
+            sensitive: true
+          )
+      end
+      it do
+        expect(chef_run).to umount_mount('/mnt/bar')
+          .with(
+            fstype: 'ceph',
+            device: ':/bar',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+      it do
+        expect(chef_run).to disable_mount('/mnt/bar')
+          .with(
+            fstype: 'ceph',
+            device: ':/bar',
+            options: ['_netdev', 'name=cephfs', 'secretfile=/etc/ceph/ceph.client.cephfs.secret'],
+            dump: 0,
+            pass: 0
+          )
+      end
+    end
+  end
+end
--- test/cookbooks/ceph_test/recipes/mds.rb
+++ test/cookbooks/ceph_test/recipes/mds.rb
@@ -1,7 +1,35 @@
-execute 'ceph fs new cephfs cephfs_metadata cephfs_data' do
-  not_if 'ceph fs get cephfs'
+osl_cephfs '/mnt/ceph' do
+  key 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ=='
+  client_name 'cephfs'
 end
 
-ceph_chef_cephfs '/mnt/ceph' do
-  action [:enable, :mount]
+directory '/mnt/ceph/bar'
+directory '/mnt/ceph/foo'
+
+file '/mnt/ceph/bar/foo' do
+  content 'barfoo'
+end
+
+file '/mnt/ceph/foo/bar' do
+  content 'foobar'
+end
+
+osl_cephfs '/mnt/bar' do
+  key 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ=='
+  subdir '/bar'
+  client_name 'cephfs'
+end
+
+osl_cephfs 'umount /mnt/bar' do
+  name '/mnt/bar'
+  key 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ=='
+  subdir '/bar'
+  client_name 'cephfs'
+  action :umount
+end
+
+osl_cephfs '/mnt/foo' do
+  key 'AQB3sfxaorsvKhAAkS7kVr01tZQNT1u0mhS1oQ=='
+  subdir '/foo'
+  client_name 'cephfs'
 end
