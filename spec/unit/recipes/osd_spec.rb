ecfb8009042e2f9ecb563a003024ec1da485a577
Lance Albertson <lance@osuosl.org>
2024-04-09 09:13:51 -0700

    Add bootstrap-osd keyring
    This allows us to recreate OSD's on nodes which was missing in the refactor.
    Skip creating it if the bootstrap isn't included in the databag.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/unit/recipes/osd_spec.rb
+++ spec/unit/recipes/osd_spec.rb
@@ -12,4 +12,18 @@ describe 'osl-ceph::osd' do
   it { is_expected.to install_osl_ceph_install('osd').with(osd: true) }
   it { is_expected.to enable_service 'ceph-osd.target' }
   it { is_expected.to start_service 'ceph-osd.target' }
+  it { is_expected.to_not create_osl_ceph_keyring 'bootstrap-osd' }
+
+  context 'bootstrap-osd set' do
+    cached(:subject) { chef_run }
+    override_attributes['osl-ceph']['data_bag_item'] = 'cluster'
+
+    before do
+      stub_data_bag_item('ceph', 'cluster').and_return(
+        bootstrap_key: 'bootstrap_key'
+      )
+    end
+
+    it { is_expected.to create_osl_ceph_keyring('bootstrap-osd').with(key: 'bootstrap_key') }
+  end
 end
