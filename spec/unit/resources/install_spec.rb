5502cb9584ffd87b717fe9ba3ac9f6bc30f0eb65
Lance Albertson <lance@osuosl.org>
2024-11-25 18:51:07 -0800

    Temporary workaround for issue with upstream repo
    They removed EL8 builds with 17.2.8.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/unit/resources/install_spec.rb
+++ spec/unit/resources/install_spec.rb
@@ -13,16 +13,16 @@ describe 'osl_ceph_install' do
 
   it do
     is_expected.to create_yum_repository('ceph').with(
-      description: 'Ceph quincy',
-      baseurl: 'https://download.ceph.com/rpm-quincy/el$releasever/$basearch',
+      description: 'Ceph 17.2.7',
+      baseurl: 'https://download.ceph.com/rpm-17.2.7/el$releasever/$basearch',
       gpgkey: 'https://download.ceph.com/keys/release.asc'
     )
   end
 
   it do
     is_expected.to create_yum_repository('ceph-noarch').with(
-      description: 'Ceph noarch quincy',
-      baseurl: 'https://download.ceph.com/rpm-quincy/el$releasever/noarch',
+      description: 'Ceph noarch 17.2.7',
+      baseurl: 'https://download.ceph.com/rpm-17.2.7/el$releasever/noarch',
       gpgkey: 'https://download.ceph.com/keys/release.asc'
     )
   end
@@ -36,8 +36,8 @@ describe 'osl_ceph_install' do
 
     it do
       is_expected.to create_yum_repository('ceph').with(
-        description: 'Ceph quincy',
-        baseurl: 'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/ceph-quincy/$basearch',
+        description: 'Ceph 17.2.7',
+        baseurl: 'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/ceph-17.2.7/$basearch',
         gpgkey: 'https://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl'
       )
     end
